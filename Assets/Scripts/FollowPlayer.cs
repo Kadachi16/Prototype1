9edfeee7b576deb639446a4493c1e28aeb31f16c
Kadachi16 <a47541@alunos.ipb.pt>
2026-05-05 14:46:38 +0100

    Ajuste LateUpate Camera

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,6 +4,7 @@ public class FollowPlayer : MonoBehaviour
 {
 
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
 
 
 
@@ -14,8 +15,8 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = player.transform.position + new Vector3(0, 5, -7);
+        transform.position = player.transform.position + offset;
     }
 }
